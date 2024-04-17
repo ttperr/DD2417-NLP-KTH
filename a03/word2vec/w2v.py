91b29d2722d7657783835904118c8099d2100e6c
Tristan Perrot <tt.perrot@gmail.com>
2024-04-17 21:05:02 +0200

    wip

--- a03/word2vec/w2v.py
+++ a03/word2vec/w2v.py
@@ -20,7 +20,7 @@ class Word2Vec(object):
                  learning_rate=0.025, epochs=5, use_corrected=True, use_lr_scheduling=True):
         """
         Constructs a new instance.
-        
+
         :param      filenames:      A list of filenames to be used as the training material
         :param      dimension:      The dimensionality of the word embeddings
         :param      window_size:    The size of the context window
@@ -43,32 +43,31 @@ class Word2Vec(object):
         self.__use_corrected = use_corrected
         self.__use_lr_scheduling = use_lr_scheduling
 
-
     def init_params(self, W, w2i, i2w):
         self.__W = W
         self.__w2i = w2i
         self.__i2w = i2w
 
-
     @property
     def vocab_size(self):
         return self.__V
-        
 
     def clean_line(self, line):
         """
         The function takes a line from the text file as a string,
         removes all the punctuation and digits from it and returns
         all words in the cleaned line as a list
-        
+
         :param      line:  The line
         :type       line:  str
         """
         #
         # REPLACE WITH YOUR CODE HERE
         #
-        return []
+        line = line.translate(str.maketrans('', '', string.punctuation))
+        line = line.translate(str.maketrans('', '', string.digits))
 
+        return line.split()
 
     def text_gen(self):
         """
@@ -88,11 +87,10 @@ class Word2Vec(object):
                 for line in f:
                     yield self.clean_line(line)
 
-
     def get_context(self, sent, i):
         """
         Returns the context of the word `sent[i]` as a list of word indices
-        
+
         :param      sent:  The sentence
         :type       sent:  list
         :param      i:     Index of the focus word in the sentence
@@ -100,9 +98,10 @@ class Word2Vec(object):
         """
         #
         # REPLACE WITH YOUR CODE
-        # 
-        return []
-
+        #
+        context_words = sent[max(0, i - self.__lws):i] + \
+            sent[i + 1:min(i + self.__rws + 1, len(sent))]
+        return [self.__w2i[w] for w in context_words]
 
     def skipgram_data(self):
         """
@@ -116,9 +115,8 @@ class Word2Vec(object):
         """
         #
         # REPLACE WITH YOUR CODE
-        # 
+        #
         return [], []
-
 
     def sigmoid(self, x):
         """
@@ -126,12 +124,11 @@ class Word2Vec(object):
         """
         return 1 / (1 + np.exp(-x))
 
-
     def negative_sampling(self, number, xb, pos):
         """
         Sample a `number` of negatives examples with the words in `xb` and `pos` words being
         in the taboo list, i.e. those should be replaced if sampled.
-        
+
         :param      number:     The number of negative examples to be sampled
         :type       number:     int
         :param      xb:         The index of the current focus word
@@ -143,7 +140,6 @@ class Word2Vec(object):
         # REPLACE WITH YOUR CODE
         #
         return []
-
 
     def train(self):
         """
@@ -160,32 +156,31 @@ class Word2Vec(object):
         for ep in range(self.__epochs):
             for i in tqdm(range(N)):
                 #
-                # YOUR CODE HERE 
+                # YOUR CODE HERE
                 #
                 pass
 
-
-    def find_nearest(self, words, metric):
+    def find_nearest(self, words, k=5, metric='cosine'):
         """
         Function returning k nearest neighbors with distances for each word in `words`
-        
+
         We suggest using nearest neighbors implementation from scikit-learn 
         (https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestNeighbors.html). Check
         carefully their documentation regarding the parameters passed to the algorithm.
-    
+
         To describe how the function operates, imagine you want to find 5 nearest neighbors for the words
         "Harry" and "Potter" using some distance metric `m`. 
         For that you would need to call `self.find_nearest(["Harry", "Potter"], k=5, metric='m')`.
         The output of the function would then be the following list of lists of tuples (LLT)
         (all words and distances are just example values):
-    
+
         [[('Harry', 0.0), ('Hagrid', 0.07), ('Snape', 0.08), ('Dumbledore', 0.08), ('Hermione', 0.09)],
          [('Potter', 0.0), ('quickly', 0.21), ('asked', 0.22), ('lied', 0.23), ('okay', 0.24)]]
-        
+
         The i-th element of the LLT would correspond to k nearest neighbors for the i-th word in the `words`
         list, provided as an argument. Each tuple contains a word and a similarity/distance metric.
         The tuples are sorted either by descending similarity or by ascending distance.
-        
+
         :param      words:   Words for the nearest neighbors to be found
         :type       words:   list
         :param      metric:  The similarity/distance metric
@@ -194,8 +189,11 @@ class Word2Vec(object):
         #
         # REPLACE WITH YOUR CODE
         #
-        return []
-
+        X = np.array([self.__cv[word] for word in words])
+        nbrs = NearestNeighbors(n_neighbors=k, metric=metric)
+        nbrs.fit(np.array(list(self.__cv.values())))
+        kneighbors = nbrs.kneighbors(X)
+        return [[(self.__i2w[neighbor], round(distance, 3)) for distance, neighbor in zip(distances, neighbors)] for distances, neighbors in zip(kneighbors[0], kneighbors[1])]
 
     def write_to_file(self):
         """
@@ -206,10 +204,10 @@ class Word2Vec(object):
                 W = self.__W
                 f.write("{} {}\n".format(self.__V, self.__H))
                 for i, w in enumerate(self.__i2w):
-                    f.write(w + " " + " ".join(map(lambda x: "{0:.6f}".format(x), W[i,:])) + "\n")
+                    f.write(
+                        w + " " + " ".join(map(lambda x: "{0:.6f}".format(x), W[i, :])) + "\n")
         except:
             print("Error: failing to write model to the file")
-
 
     @classmethod
     def load(cls, fname):
@@ -235,7 +233,6 @@ class Word2Vec(object):
             print("Error: failing to load the model to the file")
         return w2v
 
-
     def interact(self):
         """
         Interactive mode allowing a user to enter a number of space-separated words and
@@ -245,12 +242,11 @@ class Word2Vec(object):
         text = input('> ')
         while text != 'q':
             text = text.split()
-            neighbors = self.find_nearest(text, 'cosine')
+            neighbors = self.find_nearest(text, metric='cosine')
 
             for w, n in zip(text, neighbors):
                 print("Neighbors for {}: {}".format(w, n))
             text = input('> ')
-
 
     def train_and_persist(self):
         """
@@ -260,17 +256,22 @@ class Word2Vec(object):
         self.train()
         self.write_to_file()
         self.interact()
-        
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='word2vec embeddings toolkit')
     parser.add_argument('-t', '--text', default='harry_potter_1.txt',
                         help='Comma-separated source text files to be trained on')
-    parser.add_argument('-s', '--save', default='w2v.txt', help='Filename where word vectors are saved')
-    parser.add_argument('-d', '--dimension', default=50, help='Dimensionality of word vectors')
-    parser.add_argument('-ws', '--window-size', default=2, help='Context window size')
-    parser.add_argument('-neg', '--negative_sample', default=10, help='Number of negative samples')
-    parser.add_argument('-lr', '--learning-rate', default=0.025, help='Initial learning rate')
+    parser.add_argument('-s', '--save', default='w2v.txt',
+                        help='Filename where word vectors are saved')
+    parser.add_argument('-d', '--dimension', default=50,
+                        help='Dimensionality of word vectors')
+    parser.add_argument('-ws', '--window-size', default=2,
+                        help='Context window size')
+    parser.add_argument('-neg', '--negative_sample',
+                        default=10, help='Number of negative samples')
+    parser.add_argument('-lr', '--learning-rate',
+                        default=0.025, help='Initial learning rate')
     parser.add_argument('-e', '--epochs', default=5, help='Number of epochs')
     parser.add_argument('-uc', '--use-corrected', action='store_true', default=True,
                         help="""An indicator of whether to use a corrected unigram distribution
